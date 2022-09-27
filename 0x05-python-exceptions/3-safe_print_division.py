dcf4e78a0cd377bec8c708a57382c4e7e4bbc8a2
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-09-27 09:11:28 -0700

    Task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    num_of_elements = 0
+    for index in range(x):
+        try:
+            print("{:d}".format(my_list[index]), end="")
+            num_of_elements += 1
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return num_of_elements
