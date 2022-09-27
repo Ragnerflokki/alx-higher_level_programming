ccbb8aec8d6b481f9890a902f3d77ea5f1469f8a
Ragnerflokki <joeyins2002@yahoo.co.uk>
2022-09-27 09:25:39 -0700

    Task completed

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-def safe_print_list_integers(my_list=[], x=0):
-    num_of_elements = 0
-    for index in range(x):
-        try:
-            print("{:d}".format(my_list[index]), end="")
-            num_of_elements += 1
-        except (ValueError, TypeError):
-            pass
-
-    print()
-    return num_of_elements
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return div
